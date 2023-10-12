9006246dbd26a7b79a508ca735f8c924d423106d
pratiksha801 <pratikshalandge52@gmail.com>
2023-10-12 14:57:07 +0530

    csharp

---
+++ c_sharp/char_1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace characcept
+{
+    class A
+    {
+        public static void Main()
+        {
+            char ch;
+            Console.WriteLine("enter character");
+            ch = Convert.ToChar(Console.Read());
+            Console.WriteLine("ch = " + ch);
+            Console.ReadKey();
+
+        }
+    }
+}

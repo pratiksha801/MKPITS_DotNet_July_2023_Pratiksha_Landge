9905b233f424b7e4c736b944c0236a23a018c37c
pratiksha801 <pratikshalandge52@gmail.com>
2023-11-02 22:11:54 +0530

    csharp

---
+++ c_sharp/LibraryBook/LibraryBook/LibraryBook.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryBook
+{
+    internal class LibraryBook
+    {
+        public string title { get; set; }
+        public string author { get; set; }
+        public bool isavailable { get; set; }
+    }
+
+}
+
---
+++ c_sharp/employee _class/employee _class/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace employee__class
+{
+    internal class Employee
+    {
+
+        public int employeeid { get; set; }
+        public string name { get; set; }
+        public double salary { get; set; }
+
+    }
+}

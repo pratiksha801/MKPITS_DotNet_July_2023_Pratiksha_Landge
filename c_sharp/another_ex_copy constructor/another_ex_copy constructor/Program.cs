1d5b3770aa805ffeef3a0aabddbfa5039e409b24
pratiksha801 <pratikshalandge52@gmail.com>
2023-11-07 01:36:44 +0530

    scharp

---
+++ c_sharp/another_ex_copy constructor/another_ex_copy constructor/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    class book
+    {
+        int bookid;
+        string title;
+        //creating constructor
+        public book()
+        {
+            bookid = 123;
+            title = "vb.net";
+        }
+        //creating a copy constructor
+        public book(book b2)
+        {
+            bookid = b2.bookid;
+            title = b2.title;
+        }
+        //method to display the data
+        public void display()
+        {
+            Console.WriteLine("book id " + bookid);
+            Console.WriteLine("book title " + title);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            book book1 = new book();
+            book book2 = new book(book1);
+            Console.WriteLine("book1 details are :");
+            book1.display();
+            Console.WriteLine("book2 details are :");
+            book2.display();
+
+
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ c_sharp/constructor_another_object/constructor_another_object/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace constructor_another_object
+{
+    class book
+    {
+        int bookid;
+        string title;
+        //ceating constructor
+        public book()
+        {
+            bookid = 123;
+            title = "vb.net";
+        }
+        //creating a copy constructor
+        public book(book b2)
+        {
+            bookid = b2.bookid;
+            title = b2.title;
+        }
+        //method to display the data
+        public void display()
+        {
+            Console.WriteLine("book id " + bookid);
+            Console.WriteLine("book title " + title);
+        }
+    }
+    class program
+    {
+        static void Main(string[] args)
+        {
+            book book1 = new book();
+            book book2 = new book();
+            Console.WriteLine("book1 details are : ");
+            book1.display();
+            Console.WriteLine("book2 details are : ");
+            book2.display();
+            Console.ReadKey();
+        }
+    }
+}
+
+        
+    
+        
+        
+            

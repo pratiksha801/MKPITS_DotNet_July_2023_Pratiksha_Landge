9c32e813df7cbc528b70b0585ca6de6a0f991473
pratiksha801 <pratikshalandge52@gmail.com>
2023-11-08 23:58:12 +0530

    csharp

---
+++ c_sharp/static_constructor_ex2/static_constructor_ex2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    class book
+    {
+        static string title;
+        static book()
+        {
+            Console.WriteLine(" static constructor called");
+            title = "mkpits";
+        }
+        public static void display()
+        {
+            Console.WriteLine("title : " + title);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            book.display();
+            Console.ReadKey();
+        }
+    }
+}

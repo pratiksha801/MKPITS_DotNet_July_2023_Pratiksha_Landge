819fa049b4c7787299bbea97d24a7b94e89c7755
pratiksha801 <pratikshalandge52@gmail.com>
2023-11-25 13:12:36 +0530

    csharp

---
+++ c_sharp/Hashtable_store_the_data_key_value/Hashtable_store_the_data_key_value/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+
+namespace CollectionsApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable ht = new Hashtable();
+
+            ht.Add("001", "pratiksha");
+            ht.Add("002", "payal");
+            ht.Add("003", "arohi");
+            ht.Add("004", "amit");
+            ht.Add("005", "shivanya");
+            ht.Add("006", "sharu");
+            ht.Add("007", "neharika");
+
+            if (ht.ContainsValue("Nuha Ali"))
+            {
+                Console.WriteLine("This student name is already in the list");
+            }
+            else
+            {
+                ht.Add("008", "pratiksha");
+            }
+
+            // Get a collection of the keys.
+            ICollection key = ht.Keys;
+
+            foreach (string k in key)
+            {
+                Console.WriteLine(k + ": " + ht[k]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
---
+++ c_sharp/Hashtable_windows/Hashtable_windows/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hashtable_windows
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Hashtable ht = new Hashtable();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ht.Add(textBox1.Text, textBox2.Text);
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox1.Focus();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            ICollection key = ht.Keys;
+            foreach (string str in key)
+            {
+                sb.Append("album name " + str + "\n");
+            }
+            label3.Text = sb.ToString();
+            ICollection val = ht.Values;
+            StringBuilder sb1 = new StringBuilder();
+            foreach (string str in val)
+            {
+                sb1.Append("singer " + str + "\n");
+            }
+            label4.Text = sb1.ToString();
+        }
+    }
+
+}
+    
+

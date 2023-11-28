1a42a8d4e0c2215e09a50d9c8420a0c31a285d9c
pratiksha801 <pratikshalandge52@gmail.com>
2023-11-28 22:34:13 +0530

    csharp

---
+++ c_sharp/Base_class_method.cs/Base_class_method.cs/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp53
+{
+    class Company
+    {
+        public string Name = "mkcl";
+        public void display()
+        {
+            Console.WriteLine("Compnay name:" + Name);
+        }
+    }
+    class franchise : Company
+    {
+        public string name = "mkpits";
+        public void display()
+        {
+            base.display();
+            Console.WriteLine("franchise name: " + name);
+        }
+
+    }
+    class program
+    {
+        static void Main(string[] args)
+        {
+            franchise f = new franchise();
+            f.display();
+            Console.ReadKey();
+        }
+    }
+}

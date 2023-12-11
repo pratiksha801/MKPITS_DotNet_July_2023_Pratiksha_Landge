04c936ee4539e85589c4e8499daf5f1fe596cfe3
pratiksha801 <pratikshalandge52@gmail.com>
2023-12-11 15:39:11 +0530

    csharp

---
+++ c_sharp/multiple_accnt_coustomer_bank_acct/multiple_accnt_coustomer_bank_acct/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multiple_accnt_coustomer_bank_acct
+{
+    class Bank
+    {
+
+        private List<Account> accounts = new List<Account>();
+
+        public void AddAccount(Account account)
+        {
+            accounts.Add(account);
+        }
+
+        public void RemoveAccount(Account account)
+        {
+            accounts.Remove(account);
+        }
+
+        public void DisplayAccounts(Customer customer)
+        {
+            Console.WriteLine("Accounts for :" + customer.Name);
+            foreach (var account in accounts)
+            {
+                if (account.Customer == customer)
+                {
+                    Console.WriteLine("Account number: {0}, Balance: {1}", account.Number, account.Balance);
+                }
+            }
+        }
+    }
+
+    class Account
+    {
+        public int Number { get; set; }
+        public decimal Balance { get; set; }
+        public Customer Customer { get; set; }
+
+        public Account(int number, decimal balance, Customer customer)
+        {
+            Number = number;
+            Balance = balance;
+            Customer = customer;
+        }
+
+        public void Deposit(decimal amount)
+        {
+            Balance += amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if (amount > Balance)
+            {
+                Console.WriteLine("Insufficient funds.");
+            }
+            else
+            {
+                Balance -= amount;
+            }
+        }
+    }
+
+    class Customer
+    {
+        public string Name { get; set; }
+        public List<Account> Accounts { get; set; }
+
+        public Customer(string name)
+        {
+            Name = name;
+            Accounts = new List<Account>();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char exit;
+            Bank bank = new Bank();
+            Console.WriteLine("Enter Customer Name : ");
+            Customer customer = new Customer(name: Console.ReadLine());
+            while (true)
+            {
+                Console.WriteLine("Enter Account Number : ");
+                int number1 = Convert.ToInt32(Console.ReadLine());
+                while (true)
+                {
+                    Account account = new Account(number1, 5000, customer);
+                    bank.AddAccount(account);
+                    Console.WriteLine("press D for deposit and W for withdrawl : ");
+                    char choice = Console.ReadLine()[0];
+                    if (choice == 'D')
+                    {
+                        Console.WriteLine("Enter amount :");
+                        account.Deposit(Convert.ToInt32(Console.ReadLine()));
+                    }
+                    else if (choice == 'W')
+                    {
+                        account.Withdraw(Convert.ToInt32(Console.ReadLine()));
+                    }
+                    Console.WriteLine("Do u want to exit program press y or n");
+                    exit = Console.ReadLine()[0];
+                    if (exit == 'y')
+                    {
+                        break;
+                    }
+                }
+                Console.WriteLine("Do u want to add another account press y or n");
+                exit = Console.ReadLine()[0];
+                if (exit == 'n')
+                {
+                    break;
+                }
+
+            }
+            bank.DisplayAccounts(customer);
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ c_sharp/tooltiptext_property/tooltiptext_property/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace tooltiptext_property
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.ToolTipTitle = "click me";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(71, 35);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(641, 332);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(633, 303);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "register";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.textBox2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(633, 303);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "account";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(128, 54);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 0;
+            this.toolTip1.SetToolTip(this.textBox1, "register ");
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(176, 27);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
